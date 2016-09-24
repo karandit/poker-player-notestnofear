1dc60be6a5711fcc282806800a386e97f47b269b
Karandi Tamas <tkarandi@gmail.com>
2016-09-24 10:12:46 +0200

    Rename team name

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,7 +6,7 @@ import com.google.gson.JsonElement;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "No Test, No Fear";
 
     public static int betRequest(Map<String, Object> request) {
     		Object current_buy_in = request.get("current_buy_in");
