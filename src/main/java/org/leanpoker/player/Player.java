c987493486a53ffa8db8f784f868fbcd3adb98ab
Karandi Tamas <tkarandi@gmail.com>
2016-09-24 10:11:27 +0200

    First strategy: fold

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,15 +1,19 @@
 package org.leanpoker.player;
 
-import com.google.gson.JsonElement;
-
 import java.util.Map;
+
+import com.google.gson.JsonElement;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
-    public static int betRequest(JsonElement request) {
-        return 0;
+    public static int betRequest(Map<String, Object> request) {
+    		Object current_buy_in = request.get("current_buy_in");
+    		if (current_buy_in != null) {
+    			return Integer.valueOf(current_buy_in.toString());
+    		}
+    		return 0;
     }
 
     public static void showdown(JsonElement game) {
