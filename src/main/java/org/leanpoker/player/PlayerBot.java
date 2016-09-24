3afd2608d22879c9df1b433b54c6fb6828ba0cfc
Karandi Tamas <tkarandi@gmail.com>
2016-09-24 15:23:54 +0200

    Add hazard

--- src/main/java/org/leanpoker/player/PlayerBot.java
+++ src/main/java/org/leanpoker/player/PlayerBot.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player;
 
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,34 +22,56 @@ public class PlayerBot {
     	Player ourself = game.getPlayers().get(game.getIn_action());
     	List<Card> ourCards = ourself.getHole_cards();
     	
-    	List<String> highCards = Arrays.asList("9", "10", "J", "Q", "K", "A");
+    	List<String> highRanks = Arrays.asList("9", "10", "J", "Q", "K", "A");
     	
-    	boolean haveAtLeastOneHighCard = ourCards
+    	boolean check = ourCards
     			.stream()
-    			.map(card -> card.getSuit())
-    			.anyMatch(suit -> highCards.contains(suit));
+    			.map(card -> card.getRank())
+    			.anyMatch(rank -> highRanks.contains(rank));
     
-    	Map<String, List<Card>> groupedBySuit = ourCards
-    		.stream()
-    		.collect(groupingBy(card -> card.getSuit()));
     	
-    	boolean haveAtLeastOnePair = groupedBySuit
+    	boolean hazardRaise = isHazard(game);
+    	boolean raise = isJustRaise(ourCards);
+    	
+    	if (hazardRaise) {
+    		return current_buy_in - ourself.getBet() + game.getMinimum_raise() + 100;
+		}
+    	if (raise) {
+    		return current_buy_in - ourself.getBet() + game.getMinimum_raise();
+		}
+    	if (check) {
+    		return current_buy_in - ourself.getBet();
+		}
+		return 0;
+    }
+
+	private static boolean isJustRaise(List<Card> ourCards) {
+		Map<String, List<Card>> groupedBySuit = groupedByRank(ourCards);
+    	boolean raise = groupedBySuit
     			.entrySet()
     			.stream()
     			.anyMatch(entry -> entry.getValue().size() > 2);
-    	
-    	if (haveAtLeastOnePair) {
-    		return current_buy_in + game.getMinimum_raise();
-		}
-    	
-    	if (haveAtLeastOneHighCard) {
-    		return current_buy_in;
-		}
-    	
-    	
-    	
-		return 0;
-    }
+		return raise;
+	}
+
+	private static boolean isHazard(GameState game) {
+    	Player ourself = game.getPlayers().get(game.getIn_action());
+    	List<String> ourRanks = ourself.getHole_cards().stream().map(card -> card.getRank()).collect(toList());
+    	List<String> communityRanks = game.getCommunity_cards().stream().map(card -> card.getRank()).collect(toList());
+
+		long countOfAtleatPairs = ourRanks.stream()
+			.filter(ourRank -> communityRanks.contains(ourRank))
+			.count();
+
+		return countOfAtleatPairs > 1;
+	}
+
+	private static Map<String, List<Card>> groupedByRank(List<Card> ourCards) {
+		Map<String, List<Card>> groupedBySuit = ourCards
+    		.stream()
+    		.collect(groupingBy(card -> card.getRank()));
+		return groupedBySuit;
+	}
 
     public static void showdown(JsonElement game) {
     }
--- src/main/java/org/leanpoker/player/PlayerServlet.java
+++ src/main/java/org/leanpoker/player/PlayerServlet.java
@@ -16,7 +16,7 @@ import com.google.gson.JsonParser;
 public class PlayerServlet extends HttpServlet {
 
 	private final static Gson gson = new GsonBuilder().create();
-    private static final String VERSION = "4";
+    private static final String VERSION = "5";
 
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
