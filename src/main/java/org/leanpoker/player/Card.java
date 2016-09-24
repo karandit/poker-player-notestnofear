dc2220bc5c7b7a94b1af4b95afa4e576ef447f75
Toth Zoltan (CC/ENG1-Bp) <tzt2bp@bosch.com>
2016-09-24 12:16:41 +0200

    Card

---
+++ src/main/java/org/leanpoker/player/Card.java
@@ -0,0 +1,21 @@
+package org.leanpoker.player;
+
+public class Card {
+	
+	private String rank;
+	private String suit;
+	
+	public String getRank() {
+		return rank;
+	}
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+	public String getSuit() {
+		return suit;
+	}
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+}
