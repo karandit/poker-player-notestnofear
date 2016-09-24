2a51d0a44c36c972f8f2654f3ef1ea9b9bcffbbe
Karandi Tamas <tkarandi@gmail.com>
2016-09-24 12:08:29 +0200

    Use the JSON parser

--- src/main/java/org/leanpoker/player/PlayerBot.java
+++ src/main/java/org/leanpoker/player/PlayerBot.java
@@ -1,7 +1,5 @@
 package org.leanpoker.player;
 
-import java.util.Map;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
@@ -11,14 +9,13 @@ public class PlayerBot {
     static final String VERSION = "No Test, No Fear";
 	private final static Gson gson = new GsonBuilder().create();
 
-    public static int betRequest(String gameState) {
-    	Map<String, Object> request = gson.fromJson(gameState, Map.class);
-    	Object current_buy_in = request.get("current_buy_in");
-    		if (current_buy_in != null) {
-    			 Double d = Double.valueOf(current_buy_in.toString());
-    			 return d.intValue();
+    public static int betRequest(String gameStateJson) {
+    	GameState request = gson.fromJson(gameStateJson, GameState.class);
+    	int current_buy_in = request.getCurrent_buy_in();
+    		if (current_buy_in != 0) {
+    			 return current_buy_in;
     		}
-    		return 15;
+    		return 1000;
     }
 
     public static void showdown(JsonElement game) {
